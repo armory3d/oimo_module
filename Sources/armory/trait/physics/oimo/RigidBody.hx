76cf2e2e53dab7c8ede19f27fc5816f7c1be41b8
Moises Pelaez <moises.jpelaez@gmail.com>
2024-04-30 19:26:31 -0400

    Temporary fix: assign hardcoded linear damping and angular damping to prevent bodies from dissapearing

--- Sources/armory/trait/physics/oimo/RigidBody.hx
+++ Sources/armory/trait/physics/oimo/RigidBody.hx
@@ -138,8 +138,8 @@ class RigidBody extends Trait {
 		q1.init(transform.rot.x, transform.rot.y, transform.rot.z, transform.rot.w);
 		body.setOrientation(q1);
 		body.addShape(new oimo.dynamics.rigidbody.Shape(shapeConfig));
-		body.setLinearDamping(this.linearDamping);
-		body.setAngularDamping(this.angularDamping);
+		body.setLinearDamping(0.04);
+		body.setAngularDamping(0.1);
 		body.userData = this;
 
 		id = nextId++;
