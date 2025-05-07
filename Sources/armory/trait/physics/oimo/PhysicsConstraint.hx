8b43ec7712072f13500b4a64c9c3be84676608a5
Moisés Peláez <moises.jpelaez@gmail.com>
2025-05-07 19:41:49 -0400

    Minor update for Nodes compatibility

--- Sources/armory/trait/physics/oimo/PhysicsConstraint.hx
+++ Sources/armory/trait/physics/oimo/PhysicsConstraint.hx
@@ -4,8 +4,12 @@ package armory.trait.physics.oimo;
 import iron.Trait;
 
 class PhysicsConstraint extends Trait {
-
 	public function new(body1:String, body2:String) { super(); }
 }
 
+@:enum abstract ConstraintAxis(Int) from Int to Int {
+	var X = 0;
+	var Y = 1;
+	var Z = 2;
+}
 #end
